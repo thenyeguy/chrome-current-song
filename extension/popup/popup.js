617fee5e04ab04f9f54604a28a66acfe22d1eb03
Michael Nye <thenyeguy@gmail.com>
2016-07-25 20:42:28 -0700

    Prettify the popup

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,20 +1,56 @@
 "use strict";
 
-var engineApi = chrome.extension.getBackgroundPage().engineApi;
+function formatTime(time) {
+    var minutes = Math.floor(time / 60);
+    var seconds = time % 60;
+    if (seconds < 10) {
+        seconds = "0"+seconds;
+    }
+    return minutes + ":" + seconds;
+}
+
+function drawArt(artUri) {
+    if (artUri) {
+        $("#art-icon").hide();
+        $("#art-image").show();
+        $("#art-image").attr("src", artUri);
+    } else {
+        $("#art-icon").show();
+        $("#art-image").hide();
+    }
+}
+
+function drawPlaybar(playtime, length) {
+    var width = 100 * playtime / length;
+    $("#playbar .progress-bar").attr("aria-valuenow", playtime);
+    $("#playbar .progress-bar").attr("aria-valuemax", length);
+    $("#playbar .progress-bar").css("width", width+"%");
+}
 
 function update() {
-    var state = engineApi.getPlayerState();
-    if(state) {
-        $("#source").text(state.source);
-        $("#song").text(state.title);
-        $("#artist").text(state.artist);
-        $("#album").text(state.album);
-        $("#playtime").text(state.playtime + "/" + state.length);
-        $("#state").text(state.playing ? "playing" : "paused");
+    var state = window.engineApi.getPlayerState();
+    if (state) {
+        $("#nothing").hide();
+        $("#nowplaying").show();
+
+        $("#state-title").text(state.title);
+        $("#state-artist").text(state.artist);
+        $("#state-album").text(state.album);
+        $("#state-source").text(state.source);
+
+        $("#state-playtime").text(formatTime(state.playtime));
+        $("#state-length").text(formatTime(state.length));
+
+        drawArt(state.art_uri);
+        drawPlaybar(state.playtime, state.length);
+    } else {
+        $("#nothing").show();
+        $("#nowplaying").hide();
     }
     setTimeout(update, 1000);
 }
 
 $(document).ready(function() {
+    window.engineApi = chrome.extension.getBackgroundPage().engineApi;
     update();
 });
