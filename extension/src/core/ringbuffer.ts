584da744acd683b6cd14913adb30f2f7b17995e0
Michael Nye <thenyeguy@gmail.com>
2016-09-25 16:21:52 -0700

    Add basic ringbuffer type

---
+++ extension/src/core/ringbuffer.ts
@@ -0,0 +1,59 @@
+class Ringbuffer<T> implements Iterable<T> {
+    private size: number;
+    private start: number;
+    private end: number;
+    private buffer: Array<T>;
+
+    constructor(size: number) {
+        this.size = size;
+        this.start = 0;
+        this.end = 0;
+        this.buffer = Array(size).fill(null);
+    }
+
+    public length(): number {
+        return this.end - this.start;
+    }
+
+    public push(item: T) {
+        this.buffer[this.end % this.size] = item;
+        ++this.end;
+        if (this.end - this.start > this.size) {
+            ++this.start;
+        }
+    }
+
+    public pop(): T {
+        if (this.start == this.end) {
+            return null;
+        }
+        let item = this.buffer[this.start];
+        ++this.start;
+        return item;
+    }
+
+    public clear() {
+        this.start = this.end;
+    }
+
+    [Symbol.iterator](): Iterator<T> {
+        let pos = this.start;
+        let ringbuffer = this;
+        return {
+            next(): IteratorResult<T> {
+                if (pos >= ringbuffer.end) {
+                    return {
+                        done: true,
+                    }
+                } else {
+                    let i = pos % ringbuffer.size;
+                    ++pos;
+                    return {
+                        done: false,
+                        value: ringbuffer.buffer[i],
+                    }
+                }
+            }
+        }
+    }
+}
