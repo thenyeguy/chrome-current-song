7ec99c6cf73cf24be765286d16a2b6d66389fdc3
Michael Nye <thenyeguy@gmail.com>
2016-11-25 14:52:25 -0800

    Fix indent

--- src/adapters/listener.ts
+++ src/adapters/listener.ts
@@ -27,61 +27,61 @@ function trimmed(str: string): string {
 
 
 class Listener {
-  private adapter: Adapter;
-  private port: chrome.runtime.Port;
+    private adapter: Adapter;
+    private port: chrome.runtime.Port;
 
-  public verbose: boolean;
+    public verbose: boolean;
 
-  constructor(adapter: Adapter) {
-      this.adapter = adapter;
-      this.port = chrome.runtime.connect(null, {
-          name: Math.random().toString(36).substr(2),
-      });
-      this.port.postMessage({ "properties": adapter.properties });
-      this.verbose = false;
-  }
+    constructor(adapter: Adapter) {
+        this.adapter = adapter;
+        this.port = chrome.runtime.connect(null, {
+            name: Math.random().toString(36).substr(2),
+        });
+        this.port.postMessage({ "properties": adapter.properties });
+        this.verbose = false;
+    }
 
-  private handleRequest(request: any) {
-      if (this.verbose) { console.log("Got request: %O", request); }
+    private handleRequest(request: any) {
+        if (this.verbose) { console.log("Got request: %O", request); }
 
-      if (request.type == "control") {
-          if (request.control == ControlType.PlayPause) {
-              this.adapter.playPause();
-          } else if (request.control == ControlType.NextSong) {
-              this.adapter.nextSong();
-          } else if (request.control == ControlType.PrevSong) {
-              this.adapter.prevSong();
-          }
-      }
-  }
+        if (request.type == "control") {
+            if (request.control == ControlType.PlayPause) {
+                this.adapter.playPause();
+            } else if (request.control == ControlType.NextSong) {
+                this.adapter.nextSong();
+            } else if (request.control == ControlType.PrevSong) {
+                this.adapter.prevSong();
+            }
+        }
+    }
 
-  private update() {
-      let msg = {}
-      let state: PlayerState = {
-          player: this.adapter.properties.name,
-          track: {
-              title: trimmed(this.adapter.getTitle()),
-              artist: trimmed(this.adapter.getArtist()),
-              album: trimmed(this.adapter.getAlbum()),
-          },
-          playState: this.adapter.getPlayState(),
-          playtime: asSeconds(this.adapter.getPlaytime()),
-          duration: asSeconds(this.adapter.getDuration()),
-          artUri: this.adapter.getArtUri(),
-      };
-      if (this.verbose) { console.log("Sending state: %O", state); }
-      this.port.postMessage({ "state": state });
-  }
+    private update() {
+        let msg = {}
+        let state: PlayerState = {
+            player: this.adapter.properties.name,
+            track: {
+                title: trimmed(this.adapter.getTitle()),
+                artist: trimmed(this.adapter.getArtist()),
+                album: trimmed(this.adapter.getAlbum()),
+            },
+            playState: this.adapter.getPlayState(),
+            playtime: asSeconds(this.adapter.getPlaytime()),
+            duration: asSeconds(this.adapter.getDuration()),
+            artUri: this.adapter.getArtUri(),
+        };
+        if (this.verbose) { console.log("Sending state: %O", state); }
+        this.port.postMessage({ "state": state });
+    }
 
-  private poll(timeout_ms: number) {
-      this.update();
-      setTimeout(this.poll.bind(this, timeout_ms), timeout_ms);
-  }
+    private poll(timeout_ms: number) {
+        this.update();
+        setTimeout(this.poll.bind(this, timeout_ms), timeout_ms);
+    }
 
-  public start(verbose?: boolean) {
-      console.log("Starting %s listener...", this.adapter.properties.name);
-      this.verbose = !!verbose;
-      this.port.onMessage.addListener(this.handleRequest.bind(this));
-      this.poll(500 /* ms */);
-  }
+    public start(verbose?: boolean) {
+        console.log("Starting %s listener...", this.adapter.properties.name);
+        this.verbose = !!verbose;
+        this.port.onMessage.addListener(this.handleRequest.bind(this));
+        this.poll(500 /* ms */);
+    }
 }
