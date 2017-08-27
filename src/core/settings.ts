c0b8aeefc9a9307557403fb926c7aaebb9fd2e7d
Michael Nye <thenyeguy@gmail.com>
2017-08-27 15:43:53 -0700

    Fix settings load

--- src/core/settings.ts
+++ src/core/settings.ts
@@ -5,27 +5,33 @@ class SettingsManager {
     private _lastFmAuthUser = "";
 
     constructor() {
-        chrome.storage.onChanged.addListener(this.handleChange.bind(this));
         this.loadSettings();
+        chrome.storage.onChanged.addListener(this.handleChange.bind(this));
     }
 
     private handleChange(changes: any, area: string) {
         for (let key in changes) {
             let change = changes[key];
             let value = change.newValue;
-            if (key == "settings.last_fm.auth_token") {
-                this._lastFmAuthToken = value;
-            } else if (key == "settings.last_fm.auth_user") {
-                this._lastFmAuthUser = value;
-            }
+            this.read(key, change.newValue);
         }
     }
 
     private loadSettings() {
         chrome.storage.sync.get(null, (items) => {
-            this.handleChange(items, "");
+            for (let key in items) {
+                this.read(key, items[key]);
+            }
             console.debug("Loaded settings.");
         });
+    }
+
+    private read(key: string, value: any) {
+        if (key == "settings.last_fm.auth_token") {
+            this._lastFmAuthToken = value;
+        } else if (key == "settings.last_fm.auth_user") {
+            this._lastFmAuthUser = value;
+        }
     }
 
     private write(field: string, value: any) {
